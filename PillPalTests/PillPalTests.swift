32afe2aff71f07c6e88531b9b1995c16e372c85d
KevinYoung23 <yingkai.yang19@imperial.ac.uk>
2026-02-11 14:33:04 +0800

    Initial Commit

---
+++ PillPalTests/PillPalTests.swift
@@ -0,0 +1,16 @@
+//
+//  PillPalTests.swift
+//  PillPalTests
+//
+//  Created by Yang, Yingkai on 11/02/2026.
+//
+
+import Testing
+
+struct PillPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
